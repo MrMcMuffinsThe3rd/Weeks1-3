550354c0b410c12e460eed40770aeec6806ca014
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-01-14 09:54:31 -0500

    Created a new Scene and demonstrated how Linear Interpolation works

---
+++ Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //public Vector2 startPoition;
+    public Transform start;
+
+    //public Vector2 endPoition;
+    public Transform end;
+
+    public float t = 0; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        if (t > 1) 
+        {
+            t = 0;
+        }
+
+        //transform.position = Vector2.Lerp(startPoition, endPoition, t);
+        transform.position = Vector2.Lerp(start.position, end.position, t);
+
+    }
+}
