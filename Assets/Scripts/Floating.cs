7556999f344c3699eb41bdaec37e7529e2feaa0a
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-01-27 17:09:05 -0500

    Added perlin noise to Floating script to make it look like the alien is floating up and down

--- Assets/Scripts/Floating.cs
+++ Assets/Scripts/Floating.cs
@@ -2,6 +2,21 @@ using UnityEngine;
 
 public class Floating : MonoBehaviour
 {
+
+    public float speed;
+
+    float x;
+
+    //lerp start/end object variables
+    public Transform start;
+    public Transform end;
+
+    //time variable
+    public float t = 0;
+
+    //animation curve variable
+    public AnimationCurve curve;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +26,27 @@ public class Floating : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime * speed;
+        if (t > 0.5) //decreased so that the alien is always behind the astronaut
+        {
+            t = 0;
+        }
+
+        //lerps the movement of the monster between two positions relative to time
+        transform.position = Vector2.Lerp(start.position, end.position, t);
+
+       float y = curve.Evaluate(t);
+
+        //makes the mosnter float up and down using perlin noise
+        Vector2 newYPos = transform.position;
+        newYPos.y = Mathf.PerlinNoise(x,y) * t; //attempt at perlin noise usage
+        transform.position = newYPos;
+
+
+
+        //left to do:
+        //add perlin noise to ufo
+        //set light to "spot"
+        //done
     }
 }
--- Assets/Scripts/Running.cs
+++ Assets/Scripts/Running.cs
@@ -13,7 +13,7 @@ public class Running : MonoBehaviour
     public Transform end;
 
     //time variable
-    public float t = 0.01f;
+    public float t = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,21 +25,13 @@ public class Running : MonoBehaviour
     void Update()
     {
 
-        t += Time.deltaTime * speed;
+        t += Time.deltaTime * speed; //slows down the movement of the astronaut
         if (t > 1)
         {
             t = 0;
         }
 
+        //lerps the movement of the astronaut between two positions relative to time
         transform.position = Vector2.Lerp(start.position, end.position, t);
-
-
-
-        //left to do:
-        //add easing to astro running
-        //do the same for monster floating
-        //add perlin noise to ufo
-        //set light to "spot"
-        //done
     }
 }
